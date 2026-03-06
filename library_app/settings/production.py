7e18a4e978ee19df24fd244e9595be97d82746d4
skholmatov <skholmatov0@gmail.com>
2026-03-06 12:15:45 +0500

    config

--- library_app/settings/production.py
+++ library_app/settings/production.py
@@ -2,12 +2,20 @@ from .development import *
 
 DEBUG = False
 
-SECURE_SSL_REDIRECT = False  # nginx handles SSL termination; no cert configured
-SESSION_COOKIE_SECURE = False  # no HTTPS; Secure flag would block cookies over HTTP
-CSRF_COOKIE_SECURE = False    # no HTTPS; Secure flag would block cookies over HTTP
-SECURE_HSTS_SECONDS = 0       # disabled: HSTS + no HTTPS locks browsers out of the site
+# HTTPS is terminated by the outer nginx-proxy; Django receives HTTP internally
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = False
+
+# Cookies must be secure now that HTTPS is in place
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Required by Django 4.0+ for CSRF validation when behind an HTTPS proxy
+CSRF_TRUSTED_ORIGINS = ['https://app.bxm-group.com']
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
 
 LOGGING = {
